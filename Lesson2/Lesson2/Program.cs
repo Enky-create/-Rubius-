7b9e62b62e0e5c84ab7dc7c4dccea39b2e50a719
Enky-create <komkowvadim@gmail.com>
2024-02-04 17:56:46 +0200

    lesson 2

---
+++ Lesson2/Lesson2/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+Console.WriteLine("Enter the year");
+string yearString = Console.ReadLine();
+
+if (!int.TryParse(yearString, out int year ) || year<0)
+{
+    Console.WriteLine("You entered not number or empty string or it was negative number");
+    return;
+}
+
+if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
