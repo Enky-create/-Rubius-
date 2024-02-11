00b201e3f8f46832734a21a59818ff9924d3e976
Enky-create <komkowvadim@gmail.com>
2024-02-11 21:30:14 +0200

    lesson4

---
+++ Lesson4/Lesson4/Caffee.cs
@@ -0,0 +1,22 @@
+﻿namespace Lesson4
+{
+    internal class Caffee
+    {
+        private List<Guest> guests;
+
+        public Caffee()
+        {
+            guests = new List<Guest>();
+        }
+
+        public void AddGuest(Guest guest)
+        {
+            guests.Add(guest);
+        }
+
+        public void ServeOrder(string order, Guest guest)
+        {
+            Console.WriteLine($"Кафе получило заказ \"{order}\" от гостя {guest.Name}");
+        }
+    }
+}
---
+++ Lesson4/Lesson4/Guest.cs
@@ -0,0 +1,19 @@
+﻿namespace Lesson4
+{
+    internal class Guest
+    {
+        public string Name { get; }
+        private Caffee _caffee;
+
+        public Guest(string name, Caffee caffee)
+        {
+            Name = name;
+            _caffee = caffee;
+        }
+
+        public void MakeOrder(string order)
+        {
+            _caffee.ServeOrder(order, this);
+        }
+    }
+}
